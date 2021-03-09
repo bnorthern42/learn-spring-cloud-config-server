cf153ab691b868d7e20926d578d993742d0b04c1
bnorthern42 <banorthern42@tntech.edu>
2021-03-09 12:48:38 -0600

    Initial commit

---
+++ src/main/java/io/learning/learnspringcloudconfigserver/LearnSpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package io.learning.learnspringcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class LearnSpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringCloudConfigServerApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/io/learning/learnspringcloudconfigserver/LearnSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.learning.learnspringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
